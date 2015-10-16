0f6fe2d27758fa60bc83922331e4f7fd01e185bf
Tim van Steenis <tims@q42.nl>
2015-10-16 10:37:20 +0200

    first version of the image preview

---
+++ src/Q42.ImagePreview/ImageConverter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+
+
+namespace Q42.ImagePreview
+{
+
+  public class ImageConverter
+  {
+    public PreviewImage? GetPreviewImage(Image originalImage, int width, int height)
+    {
+      var destinationRect = new Rectangle(0, 0, width, height);
+      var destinationImage = new Bitmap(width, height);
+
+      destinationImage.SetResolution(originalImage.HorizontalResolution, originalImage.VerticalResolution);
+
+      using (var graphics = Graphics.FromImage(destinationImage))
+      {
+        graphics.CompositingMode = CompositingMode.SourceCopy;
+        graphics.CompositingQuality = CompositingQuality.HighSpeed;
+        graphics.InterpolationMode = InterpolationMode.Low;
+        graphics.SmoothingMode = SmoothingMode.None;
+        graphics.PixelOffsetMode = PixelOffsetMode.None;
+
+        using (var wrapMode = new ImageAttributes())
+        {
+          wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+
+          var x = 0;
+          var y = 0;
+          var w = 0;
+          var h = 0;
+          if (originalImage.Width > originalImage.Height)
+          {
+            x = (originalImage.Width - originalImage.Height) / 2;
+            w = h = originalImage.Height;
+          }
+          else if (originalImage.Height > originalImage.Width)
+          {
+            y = (originalImage.Height - originalImage.Width) / 2;
+            w = h = originalImage.Width;
+          }
+
+          graphics.DrawImage(originalImage, destinationRect, x, y, w, h, GraphicsUnit.Pixel, wrapMode);
+        }
+      }
+
+      using (var memStream = new System.IO.MemoryStream())
+      {
+        var encoder = ImageCodecInfo.GetImageDecoders().FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
+        var encodingParams = new EncoderParameters(1);
+        encodingParams.Param[0] = new EncoderParameter(Encoder.Quality, 50L);
+
+        destinationImage.Save(memStream, encoder, encodingParams);
+
+        return PreviewImage.FromBytes(memStream.ToArray());
+      }
+
+    }
+
+
+  }
+}
---
+++ src/Q42.ImagePreview/PreviewImage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q42.ImagePreview
+{
+  public struct PreviewImage
+  {
+    public byte[] Header { get; set; }
+    public byte[] Body { get; set; }
+
+    public static PreviewImage? FromBytes(byte[] image)
+    {
+      var pattern = new byte[] { 0xFF, 0xDA };
+      var index = IndexOfPattern(image, pattern);
+
+      if (!index.HasValue)
+        return null;
+
+      return new PreviewImage()
+      {
+        Header = image.Take(index.Value + 1).ToArray(),
+        Body = image.Skip(index.Value).ToArray()
+      };
+    }
+
+    private static int? IndexOfPattern(IList<byte> bytes, IList<byte> pattern)
+    {
+      for (var i = 0; i < bytes.Count - 1; i++)
+      {
+        var current = bytes[i];
+        var next = bytes[i + 1];
+
+        if (current == pattern[0] && next == pattern[1])
+        {
+          return i + 2;
+        }
+      }
+
+      return null;
+    }
+  }
+}
