77f06f5c73b9e6d8a3ca739d50af74d59d95b485
Tim van Steenis <tims@q42.nl>
2015-10-19 10:59:20 +0200

    Refactor

---
+++ src/Q42.ImagePreview/ImagePreview.cs
@@ -0,0 +1,15 @@
+﻿namespace Q42.ImagePreview
+{
+  internal struct ImagePreview
+  {
+    /// <summary>
+    /// Public header
+    /// </summary>
+    public byte[] Header { get; set; }
+
+    /// <summary>
+    /// Image body (store this)
+    /// </summary>
+    public byte[] Body { get; set; }
+  }
+}
--- src/Q42.ImagePreview/ImagePreviewConverter.cs
+++ src/Q42.ImagePreview/ImagePreviewConverter.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 
 
 namespace Q42.ImagePreview
@@ -15,78 +14,84 @@ namespace Q42.ImagePreview
     private static readonly byte[] SOSPattern = { 0xFF, 0xDA };
     private const int HeaderSizeLength = 4;
 
-    private static byte[] header;
+    private static byte[] _header;
+
+    /// <summary>
+    /// Public header shared by all the images generated with this converter.
+    /// </summary>
     public static byte[] Header
     {
       get
       {
-        if (header != null)        
-          return header;        
+        if (_header != null)        
+          return _header;        
 
         var bitmap = new Bitmap(40, 40);
-        var preview = CreatePreviewImage(bitmap);
+        var preview = ImagePreviewFromImage(bitmap);
         if (!preview.HasValue)        
           throw new ImagePreviewException("Something went wrong generating the header");
 
-        return header = preview.Value.Header;
+        return _header = preview.Value.Header;
       }
     }
 
-    public static string Base64ImageFromBody(byte[] body)
+    public static string Base64Header
     {
+      get { return Convert.ToBase64String(Header); }
+    }
+
+    /// <summary>
+    /// Get the full preview image as byte[]
+    /// </summary>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    public static byte[] ByteImageFromBody(byte[] body)
+    {
+      if (body == null)
+        return null;
+
       var indexC0 = IndexOfPattern(Header, SOF0Pattern);
       if (!indexC0.HasValue)
         return null;
 
-      var headerSizeIndexStart = indexC0.Value + 5;   
-        
+      var headerSizeIndexStart = indexC0.Value + 5;
 
-      var bytes = new[]
+      return new[]
       {
         Header.Take(headerSizeIndexStart).ToArray(),
         body.Take(HeaderSizeLength).ToArray(),
         Header.Skip(headerSizeIndexStart).Take(Header.Length - headerSizeIndexStart).ToArray(),
         body.Skip(HeaderSizeLength).Take(body.Length - HeaderSizeLength).ToArray()
-      }.SelectMany(z => z).ToArray();
-       
-      return Convert.ToBase64String(bytes);
+      }.SelectMany(z => z).ToArray();      
     }
 
-
-    private static PreviewImage? PreviewImageFromBytes(byte[] image)
+    /// <summary>
+    /// Get the base64 encoded string of the full preview image.
+    /// </summary>
+    /// <param name="body"></param>
+    /// <returns></returns>
+    public static string Base64ImageFromBody(byte[] body)
     {
-      var indexC0 = IndexOfPattern(image, SOF0Pattern);
-      if (!indexC0.HasValue)
-        return null;      
-
-      var indexBodyStart = IndexOfPattern(image, SOSPattern);
-      if (!indexBodyStart.HasValue)
+      if (body == null)
         return null;
 
-      var headerSizeIndexStart = indexC0.Value + 5;
-      var headerSizeIndexEnd = headerSizeIndexStart + HeaderSizeLength;
+      var byteImage = ByteImageFromBody(body);
+      return byteImage == null ? null : Convert.ToBase64String(byteImage);
+    }
+    
+    /// <summary>
+    /// Creates a preview image
+    /// </summary>
+    /// <param name="originalImage"></param>
+    /// <returns>The bytes that you need to save. Combine these bytes with the public header to create the full preview image.</returns>
+    public static byte[] CreateImagePreview(Image originalImage)
+    {
+      var previewImage = ImagePreviewFromImage(originalImage);
+      return previewImage.HasValue ? previewImage.Value.Body : null;
 
-      // copy the first part of the header
-      var header = new[]
-      {
-        image.Take(headerSizeIndexStart).ToArray(),
-        image.Skip(headerSizeIndexEnd).Take(indexBodyStart.Value - headerSizeIndexEnd).ToArray()
-      }.SelectMany(arr => arr).ToArray();
-
-      var body = new[]
-      {
-        image.Skip(headerSizeIndexStart).Take(HeaderSizeLength).ToArray(),
-        image.Skip(indexBodyStart.Value).Take(image.Length - indexBodyStart.Value).ToArray()
-      }.SelectMany(arr => arr).ToArray();
-      
-      return new PreviewImage()
-      {
-        Header = header,
-        Body = body
-      };      
     }
 
-    public static PreviewImage? CreatePreviewImage(Image originalImage)
+    private static ImagePreview? ImagePreviewFromImage(Image originalImage)
     {
       const int maxSize = 40;
 
@@ -117,14 +122,50 @@ namespace Q42.ImagePreview
       using (var memStream = new System.IO.MemoryStream())
       {
         var encoder = ImageCodecInfo.GetImageDecoders().FirstOrDefault(codec => codec.FormatID == ImageFormat.Jpeg.Guid);
+        if (encoder == null)
+          throw new ImagePreviewException("Jpeg decoder not found");
+
         var encodingParams = new EncoderParameters(1);
         encodingParams.Param[0] = new EncoderParameter(Encoder.Quality, 50L);
 
         destinationImage.Save(memStream, encoder, encodingParams);
 
-        return PreviewImageFromBytes(memStream.ToArray());
+        return ImagePreviewFromBytes(memStream.ToArray());        
       }
+    }
 
+
+    private static ImagePreview? ImagePreviewFromBytes(byte[] image)
+    {
+      var indexC0 = IndexOfPattern(image, SOF0Pattern);
+      if (!indexC0.HasValue)
+        return null;
+
+      var indexBodyStart = IndexOfPattern(image, SOSPattern);
+      if (!indexBodyStart.HasValue)
+        return null;
+
+      var headerSizeIndexStart = indexC0.Value + 5;
+      var headerSizeIndexEnd = headerSizeIndexStart + HeaderSizeLength;
+
+      // copy the first part of the header
+      var header = new[]
+      {
+        image.Take(headerSizeIndexStart).ToArray(),
+        image.Skip(headerSizeIndexEnd).Take(indexBodyStart.Value - headerSizeIndexEnd).ToArray()
+      }.SelectMany(arr => arr).ToArray();
+
+      var body = new[]
+      {
+        image.Skip(headerSizeIndexStart).Take(HeaderSizeLength).ToArray(),
+        image.Skip(indexBodyStart.Value).Take(image.Length - indexBodyStart.Value).ToArray()
+      }.SelectMany(arr => arr).ToArray();
+
+      return new ImagePreview()
+      {
+        Header = header,
+        Body = body
+      };
     }
 
     private static int? IndexOfPattern(IList<byte> bytes, IList<byte> pattern)
